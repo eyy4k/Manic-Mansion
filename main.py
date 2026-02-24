66e6018fe1f6f34895e47ee3e517c1b644fa8eef
eyy4k <eolbergsveen@gmail.com>
2026-02-24 13:31:52 +0100

    e

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame as pg
+from constants import *
+
+pg.init()
+vindu = pg.display.set_mode([VINDU_BREDDE, VINDU_HOYDE])
+clock = pg.time.Clock()
+
+
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+            running = False
+
+    # Tegn bakgrunn: (En slags "reset" av hele vinduet vårt)
+    vindu.fill(WHITE)
+
+    # TODO: Oppdater objektene våre:
+
+
+
+
+    # Har alltid disse med til slutt:
+    pg.display.flip()
+    clock.tick(FPS)
+
+
+# While running er slutt: Avslutt pygame på en "ryddig måte":
+pg.quit()
