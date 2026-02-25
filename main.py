cfe11ec2ec9244422348b6fc031a34f7b237c118
eyy4k <eolbergsveen@gmail.com>
2026-02-25 10:17:44 +0100

    s

--- main.py
+++ main.py
@@ -7,22 +7,19 @@ vindu = pg.display.set_mode([VINDU_BREDDE, VINDU_HOYDE])
 clock = pg.time.Clock()
 
 
+
 running = True
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-        elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
-            running = False
 
-    
     vindu.fill(BLACK)
 
+    
 
 
     pg.display.flip()
     clock.tick(FPS)
 
-
-# While running er slutt: Avslutt pygame på en "ryddig måte":
 pg.quit()
--- objekt.py
+++ objekt.py
@@ -1,12 +1,40 @@
-import pygame
+import pygame as pg
+from constants import *
+
+
+class Spøkelse:
+    def __init__(self, x: int, y: int):
+        self.image = pg.image.load("spøkelse.png").convert_alpha()
+        self.image = pg.transform.scale(
+            self.image,
+            (75, 75)
+    
+        )
+        self.rect = self.image.get_rect(topleft=(x, y))
+
+
+    def tegn(self, vindu: pg.Surface):
+        vindu.blit(self.image, self.rect)
+
+        
 
 
 
-hindringer = [
 
-    pygame.Rect(100, 100, 80, 80)
+"""
 
+        self.speed = 4
+    def move(self):
+        if keys[pg.K_w]:
+            self.rect.y -= self.speed
+        if keys[pg.K_s]:
+            self.rect.y += self.speed
+        if keys[pg.K_a]:
+            self.rect.x -= self.speed
+        if keys[pg.K_d]:
+            self.rect.x += self.speed
 
-]
-
-
+    def draw(self, vindu):
+        vindu.blit(self.image, self.rect)
+        
+"""
