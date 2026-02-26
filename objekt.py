c20135764140b2952c864198fc1a71a44e05f4b8
eyy4k <eolbergsveen@gmail.com>
2026-02-26 10:20:32 +0100

    spiller

--- objekt.py
+++ objekt.py
@@ -17,3 +17,39 @@ class Spøkelse:
         vindu.blit(self.image, self.rect)
 
 
+
+
+
+
+
+class Spiller: 
+    def __init__(self, x: int, y: int):
+        self.image = pg.image.load("pacman.png").convert_alpha()
+        self.image = pg.transform.scale(self.image,
+            (75, 75)
+    
+        )
+        
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+        self.speed = 4
+    def move(self):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_w]:
+            self.rect.y -= self.speed
+        if keys[pg.K_s]:
+            self.rect.y += self.speed
+        if keys[pg.K_a]:
+            self.rect.x -= self.speed
+        if keys[pg.K_d]:
+            self.rect.x += self.speed
+
+    def draw(self, vindu: pg.surface):
+        vindu.blit(self.image, self.rect)
+        
+
+
+
+
+
