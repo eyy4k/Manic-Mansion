a455d109a81d17ce98f030a92c748e4bd3df73b3
eyy4k <eolbergsveen@gmail.com>
2026-02-26 10:32:02 +0100

    fiksing

--- objekt.py
+++ objekt.py
@@ -45,10 +45,9 @@ class Spiller:
         if keys[pg.K_d]:
             self.rect.x += self.speed
 
-    def draw(self, vindu: pg.Surface):
+    def tegnspiller(self, vindu: pg.Surface):
         vindu.blit(self.image, self.rect)
         
-
 
 
 
